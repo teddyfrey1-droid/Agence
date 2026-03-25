4f8133fec72c79268b2863af1ca24cd506d937db
Lafayette-progress <teddy.frey1@gmail.com>
2026-03-25 03:08:48 +0100

    Delete app directory

--- app/(dashboard)/app/terrain/new/page.tsx
+++
@@ -1,7 +0,0 @@
-import { requireUser } from "@/lib/auth";
-import TerrainNewClientPage from "./terrain-new-client-page";
-
-export default async function NewTerrainPage() {
-  await requireUser();
-  return <TerrainNewClientPage />;
-}
